4bb1668e25cdcbf84fa6662730e8894946bd1eb9
diaraba <diarradiaraba9@gmail.com>
2023-06-21 13:52:12 +0000

    fourth commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,30 +4,17 @@ class OrdersController < ApplicationController
   end
 
   def new
-    ActiveRecord::Base.transaction do
-      @order = Order.new
-      @order.ordered_lists.build
-      @items = Item.all.order(:created_at)
-    end  
+    @order = Order.new
+    @order.ordered_lists.build
+    @items = Item.lock.all.order(:created_at)
   end
 
   def create
-    ActiveRecord::Base.transaction do
-      @order = current_user.orders.build(order_params)
-
-      item_id = @order.ordered_lists.first.item_id
-      existing_order = Order.lock.find_by(item_id: item_id) # Verrouillage pessimiste sur la commande spécifique
-
-      if existing_order
-        raise "Another order for the same item is being processed. Please try again later."
-      end
-      unless @order.save
-        raise ActiveRecord::Rollback
-      end 
-      @order.update_total_quantity
-      # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
-      redirect_to orders_path
-    end  
+    @order = current_user.orders.build(order_params)
+    @order.save
+    @order.update_total_quantity
+    # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
+    redirect_to orders_path
   end
 
   private
